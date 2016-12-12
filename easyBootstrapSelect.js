3b5043cf604cb63e58feafa62c913293ca4d5c2d
Rehana Yasmeen <yahyasohail@hotmail.com>
2016-12-11 18:25:28 -0600

    fixed issues

--- easyBootstrapSelect.js
+++ easyBootstrapSelect.js
@@ -1,14 +1,15 @@
 (function ($) {
   $(document).ready(function () {
     $('.replaceSelect').find('select').each(function (index) {
-			let customSelect = '<ul><li class="dropdown" style="list-style: none;"><button class="btn btn-primary dropdown dropdown-toggle" data-toggle="dropdown">' + $(this).val() + ' <b class="caret"></b></button><ul class="dropdown-menu scrollable-menu">';
+			let customSelect = '<ul><li class="dropdown" style="list-style: none;"><button class="btn btn-primary dropdown dropdown-toggle" data-toggle="dropdown">' + $(this).find('option:selected').text() + ' <b class="caret"></b></button><ul class="dropdown-menu scrollable-menu">';
 
 			$(this).find('option').each(function (index) {
+				let temp = this
 				customSelect = customSelect + '<li class="' + index + ' dropdownElement ' + ($(this).prop('disabled') ? 'disabled' : '') + '"><span class="text">' + $(this).html() + '</span></li>'
 				setTimeout(function () {
 					$('.dropdownElement.' + index).on('click', function () {
-						$(this).parents('select').val($(this).attr('value'))
-						$(this).parents('select').trigger('change')
+						$(temp).parents('select').val($(temp).attr('value'))
+						$(temp).parents('select').trigger('change')
 					})
 				}, 500)
 			})
